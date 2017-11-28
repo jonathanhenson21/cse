5719cfd97066f7514aca72566841e18bab96bc65
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2017-11-28 09:29:19 -0800

    Initial commit

---
+++ notes.py
@@ -0,0 +1,24 @@
+print ("Hello World")
+
+#nathan
+
+a = 4
+b = 3
+c = 9
+
+print( a + b )
+
+print( c * b - a )
+
+print( c** a / b )
+
+prints (13 % 12)
+
+#  the "%" sign is a modulus. if finds the reminder
+
+car_name = "the wiebe moble"
+car_type = "bmw"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print ("")
