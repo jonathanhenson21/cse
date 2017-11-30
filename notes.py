45eeddf8fd96ab21fec4133e0aadb6535c563efd
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2017-11-30 09:47:07 -0800

    add loop and random numbers 11/30/17
    my work

--- notes.py
+++ notes.py
@@ -1,91 +1,125 @@
-print ("Hello World")
+# print ("Hello World")
+#
+#  # nathan
+#
+# a = 4
+# b = 3
+# c = 9
+#
+# print( a + b )
+#
+# print( c * b - a )
+#
+# print( c** a / b )
+#
+# print(13 % 12)
+#
+# #  the "%" sign is a modulus. if finds the reminder
+#
+# car_name = "the wiebe moble"
+# car_type = "bmw"
+# car_cylinders = 8
+# car_mpg = 5000.9
+#
+# print ("I have a car %s. it's pretty nice." % car_name )
+# print ("I have a car called %s." % (car_name, car_type)) # watch the order
+# print ("what is your name?")
+#
+# name = input (">_ ")
+# print ("hello %s," % name )
+#
+# print ("hello %s," % name )
+#
+# age = input ("how are old you?")
+#
+# print ("%s?! that's old really old. you belong in a retirment home.")
+#
+#
 
- # nathan
-
-a = 4
-b = 3
-c = 9
-
-print( a + b )
-
-print( c * b - a )
-
-print( c** a / b )
-
-print(13 % 12)
-
-#  the "%" sign is a modulus. if finds the reminder
-
-car_name = "the wiebe moble"
-car_type = "bmw"
-car_cylinders = 8
-car_mpg = 5000.9
-
-print ("I have a car %s. it's pretty nice." % car_name )
-print ("i have a car called %s." % ( car_name, car_type))
-
-
-
-
-
-
-
-
-
-
-
-#Function
-
-
- def print_hw() :
-     print("hello world")
-     print ("enjoy the day.")
+# Function
 
 
-print_hw()
+#   def print_hw() :
+#      print("hello world")
+#      print ("enjoy the day.")
+#
+#
+# print_hw()
+#
+#
+#
+# def say_hi(name):
+#     print("helo %s" % name)
+#     print ("coding is great!")
+#
+# say_hi("Jonathan")
+#
+#
+# def print_age ( name, age):
+#     print ("%s is %d years old" % (name, age))
+#     age = age + 1
+#     print("next year, %s will be %d years old" % ( name, age ))
+#
+# print_age("Nathan", 14)
+#
+#
+#
+# def algebra_hw(x) :
+#     return x**3 + 4*x**2 + 7 * x - 4
+# print (algebra_hw(3))
+# print (algebra_hw(4))
+# print (algebra_hw(5))
+# print (algebra_hw(6))
+# print (algebra_hw(7))
+#
+#
+#
+#
+#  # if statements
+#
+#
+#  def grade grade_calc(percentage):
+#     if perceentage >= 90:
+#     return"A"
+# elif percentage >= 80:
+#     return "B"
+# elif percentage >= 70:
+#     return "C"
+#
+# elif pecentage >= 60:
+#     return "D"
+#
+# else:
+#     "F"
+#     print(gread_calc(59)
+
+'''write a fuction called "happy bday"
+that "sings" (prints) Happy Birthday
+ 
+ It must take one parameter called "name
+'''
+
+
+def happy_bday(name):
+    print("happy birthday to you")
+    print("happy birthday to you")
+    print("happy birthday dear " + name)
+    print("happy birthday to you ")
+
+happy_bday("jordan")
+
+    # Loop
+for num in range(10):
+        print (num + 1)
+
+
+a = 1
+while a < 10:
+    print (a)
+    a += 1
 
 
 
-def say_hi(name):
-    print("helo %s" % name)
-    print ("coding is great!")
-
-say_hi("Jonathan")
-
-
-def print_age ( name, age):
-    print ("%s is %d years old" % (name, age))
-    age = age + 1
-    print("next year, %s will be %d years old" % ( name, age ))
-
-print_age("Nathan", 14)
-
-
-
-def algebra_hw(x) :
-    return x**3 + 4*x**2 + 7 * x - 4
-print (algebra_hw(3))
-print (algebra_hw(4))
-print (algebra_hw(5))
-print (algebra_hw(6))
-print (algebra_hw(7))
-
-
-
-
- # if statements
-
-
- def grade grade_calc(percentage):
-    if perceentage >= 90:
-    return"A"
-elif percentage >= 80:
-    return "B"
-elif percentage >= 70:
-    return "C"
-
-elif pecentage >= 60:
-    return "D"
-
-else:
-    "F"
+#random numbers
+import random #this shou;d be on line 1
+print (random.randint(0,1000))
