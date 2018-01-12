ad8f82b7e783bec5893c711f01c084be4c1dc9b5
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2018-01-12 09:59:04 -0800

    WORKING ON IT
    HELP ME OUT LUCKY 7S

--- nathan guessgame.py
+++ nathan guessgame.py
@@ -23,23 +23,21 @@ print('Well, ' + myName + ', I am thinking of a number between 1 and 50')
 while guessesTaken < 6:
     print('Take a guess..')
     guess = input()
-    guess = int(guess)
-
-    guessesTaken = guessesTaken + 1
 
     if guess < number:
+    guess = int(guess)
         print('Your number guess is too low, guess again')
 
     if guess > number:
         print('Your number is too high! guess lower or something!')
 
     if guess == number:
-        break
 
-    if guess == number:
+      if guess == number:
         guessesTaken = str(guessesTaken)
         print('Good job, ' + myName + '! You guessed the right number in' + guessesTaken + 'guesses!')
 
+    guessesTaken = guessesTaken + 1
     if guess != number:
             number = str(number)
             print('Nah, The number i was thinking of was ' + number)
