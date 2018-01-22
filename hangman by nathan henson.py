ec109b727c8c592adb75d5e4f8f70c9be1939310
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2018-01-22 09:43:28 -0800

    adding more

--- hangman by nathan henson.py
+++ hangman by nathan henson.py
@@ -1,2 +1,2 @@
 the_count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
-word_bank = ['the best brand to buy is Hollister that last longer', "", "", "", "", "", "", ]
+word_bank = ['JanSport', "do you know de way", "california republic", "", "", "", "", ]
