13786f7b4a688dff286e41da8a932b2985510bab
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2018-01-29 09:26:56 -0800

    FIXING SO CODS

--- hangman by nathan henson.py
+++ hangman by nathan henson.py
@@ -3,6 +3,5 @@ the_count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ]
 word_bank = ['Do', "yOu", "KnOw", "dE", "wAY", "hOmE", "My", "BrOtHeR", "cLiCk", "ClIcK", ]
 print(word_bank)
 print(len(word_bank))
-import random
 the_word = random.choice(word_bank)
 print(random.choice(word_bank))
