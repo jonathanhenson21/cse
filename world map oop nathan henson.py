36ffea18ed06493f3978f90508d4655aa5e44230
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2018-04-11 09:00:26 -0700

    upload git

--- world map oop nathan henson.py
+++ world map oop nathan henson.py
@@ -1,28 +1,36 @@
 class Room(object):
-    def __init__(self, name, north):
+    def __init__(self, name, description, items, north, south, east, west):
         self.name = name
         self.north = north
+        self.description = description
+        self.items = items
+        self.south = south
+        self.east = east
+        self.west = west
 
     def move(self, direction):
         global current_node
         current_node = globals()[getattr(self, direction)]
 
-WEST_HOUSE = Room('north', 'south', 'west', 'east')
+
+west_house = Room('The Green House', "Insert description here", None, 'backroom', None, None, 'front_door')
+backroom = Room("Green Room", "INsert Decription here", None, 'outside', 'west_house', 'side_door', 'outside_west')
 home = Room()
-SOUTH_HOUSE = Room('north', 'south', 'west', 'east')
+SOUTH_HOUSE = Room('', 'south', 'west', 'east')
 room1 = Room()
 BLUE_HOUSE = Room('north', 'south', 'west', 'east')
 room2 = Room()
 EAST_OF_LAKE = Room('north', 'south', 'west', 'east')
 room3 = Room()
 WEST_OF_HOME = Room('north', 'south', 'west', 'east')
-current_node = home
+
+current_node = west_house
 directions = ['north', 'south', 'east', 'west']
 short_directions = ['n', 's', 'e', 'w']
 
 while True:
-    print(current_node ['NAME'])  # change
-    print(current_node['DESCRIPTION'])   # Change
+    print(current_node.name)  # change
+    print(current_node.description)   # Change
     command = input('>_').lower().strip()
     if command == 'quit':
         quit(0)
@@ -31,48 +39,8 @@ while True:
         command = directions[pos]
     if command in directions:
         try:
-      #  Change
+            current_node.move(command)
         except KeyError:
             print("You cannot go this way")
-                                else:
+    else:
         print("Command not recognized")
-
-class Characters(object):
-    def __init__(self, name, dialogue, description, action, health, death):
-        self.name = name
-        self.description = dialogue
-        self.description = description
-        self.action = action
-        self.health = health
-        self.death = death
-
-    def death(self):
-        self.death = True
-        print("% died." % self.name)
-
-    def damage(self, amount):
-        self.health = amount
-        if self.health <= 0:
-            self.health = True
-
-
-Johnny_Ghost = (140, False)
-class Person(object):
-    def __init__(self, name, age, school, smart):
-        self.name = name
-        self.age = age
-        self.school = school
-        self.smart = smart
-
-    def work(self, ):
-        print("%s goes to work" % self.name)
-
-
-class Employee(object):
-    def __init__(self, name, age, job):
-        self.name = name
-        self.age = age
-        self.job = job
-
-        def work(self, ):
-            print("%s goes to work" % self.name)
