5f192610ca474ae453fc7c744c6c90e6362759bf
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2018-04-11 09:44:26 -0700

    upload to git

--- world map oop nathan henson.py
+++ world map oop nathan henson.py
@@ -14,15 +14,15 @@ class Room(object):
 
 
 west_house = Room('The Green House', "Insert description here", None, 'backroom', None, None, 'front_door')
-backroom = Room("Green Room", "INsert Decription here", None, 'outside', 'west_house', 'side_door', 'outside_west')
-home = Room()
-SOUTH_HOUSE = Room('', 'south', 'west', 'east')
-room1 = Room()
-BLUE_HOUSE = Room('north', 'south', 'west', 'east')
-room2 = Room()
-EAST_OF_LAKE = Room('north', 'south', 'west', 'east')
-room3 = Room()
-WEST_OF_HOME = Room('north', 'south', 'west', 'east')
+backroom = Room("Green Room", "ITS HAS A LOT OF PLANTS ", None, 'outside', 'west_house', 'side_door', 'outside_west')
+SOUTH_HOUSE = Room('blue house', "look old have wood walls and a fireplace", None, 'backroom', None, None, 'front_door')
+upstair = Room("2 Floor", "it haves 3 room ", None, 'Hall''wide hall way', 'SOUTH_HOUSE, "next_room', 'down_stair', 'left_roomn')
+# room1 = Room()
+# BLUE_HOUSE = Room('north', 'south', 'west', 'east')
+# room2 = Room()
+# EAST_OF_LAKE = Room('north', 'south', 'west', 'east')
+# room3 = Room()
+# WEST_OF_HOME = Room('north', 'south', 'west', 'east')
 
 current_node = west_house
 directions = ['north', 'south', 'east', 'west']
