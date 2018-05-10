3866448256775ad648cd477a0b28f3dfe1092bdd
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2018-05-10 09:04:29 -0700

    mf ghost

--- items.py
+++ items.py
@@ -11,9 +11,17 @@ class weapon(Item):
     def __init__(self, source, name, description, weight, material):
         super(weapon, self).__init__(source, name, description, weight, material)
 
+
 class inventory(Item):
     def __init__(self, source, name, description, weight, material):
         super(inventory, self).__init__(source, name, description, weight, material)
+
+
+class drink(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(drink, self).__init__(source, name, description, weight, material)
+
+
 class drink(Item):
     def __init__(self, source, name, description, weight, material):
         super(drink, self).__init__(source, name, description, weight, material)
