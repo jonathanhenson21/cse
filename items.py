7cda95a9fa9c520364db328df5daeabb8bee71eb
jonathanhenson21 <31106175+jonathanhenson21@users.noreply.github.com>
2018-05-10 09:44:31 -0700

    upload

--- items.py
+++ items.py
@@ -7,21 +7,54 @@ class Item(object):
         self.material = material
 
 
-class weapon(Item):
+class Weapon(Item):
     def __init__(self, source, name, description, weight, material):
-        super(weapon, self).__init__(source, name, description, weight, material)
+        super(Weapon, self).__init__(source, name, description, weight, material)
 
 
-class inventory(Item):
+class Inventory(Item):
     def __init__(self, source, name, description, weight, material):
-        super(inventory, self).__init__(source, name, description, weight, material)
+        super(Inventory, self).__init__(source, name, description, weight, material)
+
+
+class Drink(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(Drink, self).__init__(source, name, description, weight, material)
+
+
+class Armor(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(Armor, self).__init__(source, name, description, weight, material)
 
 
 class drink(Item):
     def __init__(self, source, name, description, weight, material):
         super(drink, self).__init__(source, name, description, weight, material)
 
+class axe(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(axe, self).__init__(source, name, description, weight, material)
+
+class Key(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(Key, self).__init__(source, name, description, weight, material)
+
+
+class chest(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(chest, self).__init__(source, name, description, weight, material)
+
+
+class drug(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(drug, self).__init__(source, name, description, weight, material)
+
+
+class backpack(Item):
+    def __init__(self, source, name, description, weight, material):
+        super(backpack, self).__init__(source, name, description, weight, material)
+
 
 class drink(Item):
     def __init__(self, source, name, description, weight, material):
-        super(drink, self).__init__(source, name, description, weight, material)
+        super(, self).__init__(source, name, description, weight, material)
